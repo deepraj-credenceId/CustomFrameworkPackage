2bf98c2acf4954259daf05170ac1fc3100c05dc8
Deepraj Jadhav <deepraj.jadhav@credenceid.com>
2024-06-05 16:46:13 +0530

    Second Commit

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "CustomFrameworkPackage",
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CustomFrameworkPackage",
-            targets: ["CustomFrameworkPackage"]),
+            name: "CustomFramework",
+            targets: ["CustomFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -20,7 +23,7 @@ let package = Package(
 //            name: "CustomFrameworkPackageTests",
 //            dependencies: ["CustomFrameworkPackage"]),
         .binaryTarget(
-            name: "CustomFrameworkPackage",
-            path: "./Sources/CustomFrameworkPackage.xcframework")
+            name: "CustomFramework",
+            path: "./Sources/CustomFramework.xcframework")
     ]
 )
