81b1e268fb1ab526d78ec2b5cfa53c8a91ec8281
Deepraj Jadhav <deepraj.jadhav@credenceid.com>
2024-06-20 11:54:11 +0530

    Update

--- Sources/CustomFramework.xcframework/ios-arm64_x86_64-simulator/CustomFramework.framework/Headers/OpensslWrapper.h
+++ Sources/CustomFramework.xcframework/ios-arm64_x86_64-simulator/CustomFramework.framework/Headers/OpensslWrapper.h
@@ -4,14 +4,14 @@
 //
 //  Created by Vijay Radake on 06/01/23.
 //
-//
-//#ifndef OpensslWrapper_h
-//#define OpensslWrapper_h
-//
-//#import <Foundation/Foundation.h>
-//@interface OpensslWrapper: NSObject
-//
-//- (NSString *)createX509Certificate:(int)serialNumber subjectDN:(NSString *)subjectDN issuerDN:(NSString*)issuerDN publicKey:(NSData *)publicKey privateKeynew:(NSData *)privateKeynew startDate:(long)startDate endDate:(long)endDate;
-//@end
-//
-//#endif /* OpensslWrapper_h */
+
+#ifndef OpensslWrapper_h
+#define OpensslWrapper_h
+
+#import <Foundation/Foundation.h>
+@interface OpensslWrapper: NSObject
+
+- (NSString *)createX509Certificate:(int)serialNumber subjectDN:(NSString *)subjectDN issuerDN:(NSString*)issuerDN publicKey:(NSData *)publicKey privateKeynew:(NSData *)privateKeynew startDate:(long)startDate endDate:(long)endDate;
+@end
+
+#endif /* OpensslWrapper_h */
